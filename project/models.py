413af02eb5d16b2f721617aed925c3498071497c
Goroshchik Ilya <goooroshchik@gmail.com>
2022-10-08 12:42:05 +0300

    Update models

--- project/models.py
+++ project/models.py
@@ -1,4 +1,4 @@
-from project import db
+from project.extensions import db
 from datetime import datetime
 
 
@@ -6,25 +6,16 @@ class LaunchModel(db.Model):
     __table_name__ = "launches"
 
     id = db.Column(db.Integer, primary_key=True)
-    url_id = db.Column(db.Integer, unique=True, nullable=False)
     model = db.Column(db.String(128), unique=False, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow())
+    date = db.Column(db.DateTime, default=datetime.now())
     image_height = db.Column(db.Integer, unique=False, nullable=False)
     image_width = db.Column(db.Integer, unique=False, nullable=False)
     image_name = db.Column(db.String(128), unique=False, nullable=False)
     success = db.Column(db.Boolean, unique=False, nullable=False)
 
-    def __init__(
-        self,
-        model: str,
-        image_height: int,
-        image_width: int,
-        success: bool,
-        image_name: str,
-        url_id: int,
-    ):
+    def __init__(self, model: str, image_height: int, image_width: int, success: bool, image_name: str, date):
         self.model = model
-        self.url_id = url_id
+        self.date = date
         self.image_height = image_height
         self.image_width = image_width
         self.image_name = image_name
