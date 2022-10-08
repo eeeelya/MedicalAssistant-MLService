ffaa19b07c503b750182224f15e426a95b31666a
Goroshchik Ilya <goooroshchik@gmail.com>
2022-10-08 12:43:24 +0300

    Change project structure and update config

--- project/config.py
+++ project/config.py
@@ -1,7 +1,13 @@
 import os
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+AWS_KEY = os.environ.get("AWS_KEY")
+AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
+AWS_REGION = os.environ.get("AWS_REGION")
+RESULTS_BUCKET = os.environ.get("RESULTS_BUCKET")
+SOURCES_BUCKET = os.environ.get("SOURCES_BUCKET")
+MODELS_BUCKET = os.environ.get("MODELS_BUCKET")
 
 
 class Config(object):
