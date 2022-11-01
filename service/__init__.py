0cb4a44bb48b9417d167a3ac20b0ce61021a073b
Goroshchik Ilya <goooroshchik@gmail.com>
2022-11-01 16:41:38 +0300

    Change initialization of service

--- service/__init__.py
+++ service/__init__.py
@@ -1,54 +1,10 @@
-import os
-from flask import Flask, request, jsonify
+from flask import Flask
 
-from service.views import views
-from service
+from service.config import MONGODB_URI, MONGODB_DATABASE
 from pymongo import MongoClient
 
 
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/")
 
-client = MongoClient(os.environ.get("MONGODB_URL"))
-db = client["ml_service"]
-
-
-@app.route('/')
-def index():
-    return jsonify(
-        status=True,
-        message='Welcome to the Dockerized Flask MongoDB app!'
-    )
-
-
-@app.route('/todo')
-def todo():
-    _todos = db.todo.find()
-
-    item = {}
-    data = []
-    for todo in _todos:
-        item = {
-            'id': str(todo['_id']),
-            'todo': todo['todo']
-        }
-        data.append(item)
-
-    return jsonify(
-        status=True,
-        data=data
-    )
-
-
-@app.route('/todo', methods=['POST'])
-def createTodo():
-    data = request.get_json(force=True)
-    item = {
-        'todo': data['todo']
-    }
-    db.todo.insert_one(item)
-
-    return jsonify(
-        status=True,
-        message='To-do saved successfully!'
-    ), 201
+client = MongoClient(MONGODB_URI)
+db = client[MONGODB_DATABASE]
